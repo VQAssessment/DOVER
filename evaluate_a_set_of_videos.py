fe12e2c8b5da2ab39d2307ef1276cb5025809bda
TimothyHTimothy <2711123841@qq.com>
2022-11-15 15:48:28 +0800

    now evaluate on a set

---
+++ evaluate_a_set_of_videos.py
@@ -0,0 +1,120 @@
+import argparse
+import os
+import pickle as pkl
+
+import decord
+import numpy as np
+import torch
+from tqdm import tqdm
+import yaml
+
+from dover.datasets import (UnifiedFrameSampler,
+                            spatial_temporal_view_decomposition)
+from dover.models import DOVER
+
+mean, std = (torch.FloatTensor([123.675, 116.28, 103.53]), 
+             torch.FloatTensor([58.395, 57.12, 57.375]))
+
+def gaussian_rescale(pr):
+    # The results should follow N(0,1)
+    pr = (pr - np.mean(pr)) / np.std(pr)
+    return pr
+
+def uniform_rescale(pr):
+    # The result scores should follow U(0,1)
+    return np.arange(len(pr))[np.argsort(pr).argsort()] / len(pr)
+    
+
+def rescale_results(results: list, vname="undefined"):
+    dbs = {"livevqc": "LIVE_VQC", "kv1k": "KoNViD-1k", "ltest": "LSVQ_Test", "l1080p": "LSVQ_1080P",  "ytugc": "YouTube_UGC"}
+    for abbr, full_name in dbs.items():
+        with open(f"dover_predictions/val-{abbr}.pkl","rb") as f:
+            pr_labels = pkl.load(f)
+        aqe_score_set = pr_labels["resize"]
+        tqe_score_set = pr_labels["fragments"]
+        tqe_score_set_p = np.concatenate((np.array([results[0]]), tqe_score_set), 0)
+        aqe_score_set_p = np.concatenate((np.array([results[1]]), aqe_score_set), 0)
+        tqe_nscore = gaussian_rescale(tqe_score_set_p)[0]
+        tqe_uscore = uniform_rescale(tqe_score_set_p)[0]
+        print(f"Compared with all videos in the {full_name} dataset:")
+        print(f"-- the technical quality of video [{vname}] is better than {int(tqe_uscore*100)}% of videos, with normalized score {tqe_nscore:.2f}.")
+        aqe_nscore = gaussian_rescale(aqe_score_set_p)[0]
+        aqe_uscore = uniform_rescale(aqe_score_set_p)[0]
+        print(f"-- the aesthetic quality of video [{vname}] is better than {int(aqe_uscore*100)}% of videos, with normalized score {aqe_nscore:.2f}.")
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument(
+        "-o", "--opt", type=str, 
+        default="./dover.yml", 
+        help="the option file"
+    )
+    
+    ## can be your own
+    parser.add_argument(
+        "-dir", "--video_dir", type=str, 
+        default="./demo", 
+        help="the input video dir"
+    )
+    
+    parser.add_argument(
+        "-d", "--device", type=str, 
+        default="cuda", 
+        help="the running device"
+    )
+    
+    
+    args = parser.parse_args()
+    
+    
+    
+
+
+    with open(args.opt, "r") as f:
+        opt = yaml.safe_load(f)
+
+    ### Load DOVER
+    evaluator = DOVER(**opt["model"]["args"]).to(args.device)
+    evaluator.load_state_dict(torch.load(opt["test_load_path"], map_location=args.device))
+    
+    video_paths = []
+    all_results = {}
+    
+    for (root,dirs,files) in os.walk(args.video_dir, topdown=True):
+        for file in files:
+            if file.endswith(".mp4"):
+                video_paths += [os.path.join(root, file)]
+        
+    dopt = opt["data"]["val-l1080p"]["args"]
+
+    temporal_samplers = {}
+    for stype, sopt in dopt["sample_types"].items():
+        if "t_frag" not in sopt:
+                # resized temporal sampling for TQE in DOVER
+            temporal_samplers[stype] = UnifiedFrameSampler(sopt["clip_len"], sopt["num_clips"], sopt["frame_interval"])
+        else:
+                # temporal sampling for AQE in DOVER
+            temporal_samplers[stype] = UnifiedFrameSampler(sopt["clip_len"] // sopt["t_frag"], sopt["t_frag"], sopt["frame_interval"], sopt["num_clips"])
+            
+            
+    # automatically no parallel computing
+    for video_path in tqdm(video_paths):
+        video_reader = decord.VideoReader(video_path)
+
+
+        ### View Decomposition
+        views, _ = spatial_temporal_view_decomposition(video_path, 
+                                                    dopt["sample_types"],
+                                                    temporal_samplers)
+
+        for k, v in views.items():
+            num_clips = dopt["sample_types"][k].get("num_clips", 1)
+            views[k] = ((v.permute(1, 2, 3, 0) - mean) / std).permute(3, 0, 1, 2).reshape(v.shape[0], num_clips, -1, *v.shape[2:]).transpose(0,1).to(args.device)
+
+        results = [r.mean().item() for r in evaluator(views)]
+        all_results[video_path] = {"technical": results[0], "aesthetic": results[1]}
+     
+        with open(f"dover_predictions/val-custom_{args.video_dir.split('/')[-1]}.pkl","wb") as wf:
+            pkl.dump(all_results, wf)
